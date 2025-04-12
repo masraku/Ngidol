a0d2ff04c72ce033944722487deab87f7fa49a9f
masraku <masendra0303@gmail.com>
2025-04-12 22:59:38 +0700

    create 1 file and update 1 file

--- src/data/guestOptions.js
+++ src/data/guestOptions.js
@@ -1,109 +1,112 @@
 export const guestOptions = [
-    {
-      name: 'NekoDachi',
-      image: '/assets/nekodachi.jpg',
-    },
-    {
-      name: 'AuroraDream',
-      image: '/assets/aurora.jpg',
-    },
-    {
-      name: 'Victory',
-      image: '/assets/victory.jpeg',
-    },
-    {
-      name: 'JKT48',
-      image: '/assets/JKT48.svg',
-    },
-    {
-      name: 'Twenty Nine Teens',
-      image: '/assets/tnt.jpg',
-    },
-    {
-      name: 'Kohi Sekai',
-      image: '/assets/kohi.jpg',
-    },
-    {
-      name: 'Gochikara',
-      image: '/assets/gochikara.jpg',
-    },
-    {
-      name: 'Muses',
-      image: '/assets/muses.jpg',
-    },
-    {
-      name: 'Melonix',
-      image: '/assets/melonix.jpg',
-    },
-    {
-      name: 'Crescendo',
-      image: '/assets/crescendo.jpg',
-    },
-    {
-      name: 'Akemi',
-      image: '/assets/akemi.jpg',
-    },
-    {
-      name: 'Cherry Wish',
-      image: '/assets/cherrywish.jpg',
-    },
-    {
-      name: 'Nextanative',
-      image: '/assets/nexta.jpeg',
-    },
-    {
-      name: 'Shoujo Complex',
-      image: '/assets/shocom.jpg',
-    },
-    {
-      name: 'Aozora Belt',
-      image: '/assets/aozora.jpeg',
-    },
-    {
-      name: 'Tomoshibi',
-      image: '/assets/tomoshibi.jpeg',
-    },
-    {
-      name: 'Kalopsia',
-      image: '/assets/kalopsia.jpeg',
-    },
-    {
-      name: 'Eighteens +',
-      image: '/assets/18+.jpeg',
-    },
-    {
-      name: 'Kirari',
-      image: '/assets/kirari.jpeg',
-    },
-    {
-      name: 'Amai Monogatari',
-      image: '/assets/amai.jpeg',
-    },
-    {
-      name: 'Lemon',
-      image: '/assets/lemon.jpeg',
-    },
-    {
-      name: 'Magic Koi',
-      image: '/assets/magickoi.jpeg',
-    },
-    {
-      name: 'Kirin Day',
-      image: '/assets/kirin.jpeg',
-    },
-    {
-      name: 'Summer Time',
-      image: '/assets/summer.jpeg',
-    },
-    {
-      name: 'Eclipse',
-      image: '/assets/eclipse.jpeg',
-    },
-    {
-      name: 'Idolize',
-      image: '/assets/idolize.jpeg',
-    },
-    
-    // Tambahkan lainnya...
-  ];
-  
+  {
+    name: 'TBA',
+    image: '/assets/TBA.png',
+  },
+  {
+    name: 'NekoDachi',
+    image: '/assets/nekodachi.jpg',
+  },
+  {
+    name: 'AuroraDream',
+    image: '/assets/aurora.jpg',
+  },
+  {
+    name: 'Victory',
+    image: '/assets/victory.jpeg',
+  },
+  {
+    name: 'JKT48',
+    image: '/assets/JKT48.svg',
+  },
+  {
+    name: 'Twenty Nine Teens',
+    image: '/assets/tnt.jpg',
+  },
+  {
+    name: 'Kohi Sekai',
+    image: '/assets/kohi.jpg',
+  },
+  {
+    name: 'Gochikara',
+    image: '/assets/gochikara.jpg',
+  },
+  {
+    name: 'Muses',
+    image: '/assets/muses.jpg',
+  },
+  {
+    name: 'Melonix',
+    image: '/assets/melonix.jpg',
+  },
+  {
+    name: 'Crescendo',
+    image: '/assets/crescendo.jpg',
+  },
+  {
+    name: 'Akemi',
+    image: '/assets/akemi.jpg',
+  },
+  {
+    name: 'Cherry Wish',
+    image: '/assets/cherrywish.jpg',
+  },
+  {
+    name: 'Nextanative',
+    image: '/assets/nexta.jpeg',
+  },
+  {
+    name: 'Shoujo Complex',
+    image: '/assets/shocom.jpg',
+  },
+  {
+    name: 'Aozora Belt',
+    image: '/assets/aozora.jpeg',
+  },
+  {
+    name: 'Tomoshibi',
+    image: '/assets/tomoshibi.jpeg',
+  },
+  {
+    name: 'Kalopsia',
+    image: '/assets/kalopsia.jpeg',
+  },
+  {
+    name: 'Eighteens +',
+    image: '/assets/18+.jpeg',
+  },
+  {
+    name: 'Kirari',
+    image: '/assets/kirari.jpeg',
+  },
+  {
+    name: 'Amai Monogatari',
+    image: '/assets/amai.jpeg',
+  },
+  {
+    name: 'Lemon',
+    image: '/assets/lemon.jpeg',
+  },
+  {
+    name: 'Magic Koi',
+    image: '/assets/magickoi.jpeg',
+  },
+  {
+    name: 'Kirin Day',
+    image: '/assets/kirin.jpeg',
+  },
+  {
+    name: 'Summer Time',
+    image: '/assets/summer.jpeg',
+  },
+  {
+    name: 'Eclipse',
+    image: '/assets/eclipse.jpeg',
+  },
+  {
+    name: 'Idolize',
+    image: '/assets/idolize.jpeg',
+  },
+
+  // Tambahkan lainnya...
+];
